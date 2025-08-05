c0b83bb2fd2639a6690da9d331171075ff65b6ec
Muhammad Abbas Bangash <mabbasbangash97@users.noreply.github.com>
2025-08-06 02:27:54 +0500

    Update server.py

--- server.py
+++ server.py
@@ -24,4 +24,4 @@ async def get_openapi():
 
 @app.get("/hello")
 def hello():
-    return {"message": "Hello from your Copilot plugin!"}
+    return {"message": "Hello from your Copilot plugin! opro v21.4"}
