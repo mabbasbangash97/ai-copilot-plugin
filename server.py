1ecffceff5a29942af20639212ac321415dc3104
Muhammad Abbas Bangash <mabbasbangash97@users.noreply.github.com>
2025-09-16 01:51:34 +0500

    extension opro copilot
    https://orangeproai.atlassian.net/wiki/spaces/SCRUMPROJE/pages/28672001/High-Level+System+Architecture+Requirements https://mattermost.atlassian.net/browse/MM-63726 https://orangeproai.atlassian.net/browse/SCRUM-24
    https://opro.testrail.io/index.php?/cases/view/1001

--- server.py
+++ server.py
@@ -24,4 +24,4 @@ async def get_openapi():
 
 @app.get("/hello")
 def hello():
-    return {"message": "Hello from your Copilot plugin! OPRO v22 asdasdas as das casd as das ds"}
+    return {"message": "Hello from your Copilot plugin! OPRO v22 asdasd asd as dasd asd as das raw er 3ea3e"}
